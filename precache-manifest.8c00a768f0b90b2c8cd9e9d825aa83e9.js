95620a62dfdc97507ccd4246a8ee16364ed1858c
Leo J Penaloza <39505025+lpenaloza16@users.noreply.github.com>
2023-10-13 15:15:04 -0700

    core

---
+++ precache-manifest.8c00a768f0b90b2c8cd9e9d825aa83e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d677a2b7f21c40952fbf0b95c8f0c5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "681416a82ce29c44587b",
+    "url": "/static/css/main.4fc73c04.chunk.css"
+  },
+  {
+    "revision": "303595ed36b3cd71f9bc",
+    "url": "/static/js/2.ccccdf38.chunk.js"
+  },
+  {
+    "revision": "681416a82ce29c44587b",
+    "url": "/static/js/main.3d8cc3e8.chunk.js"
+  },
+  {
+    "revision": "c365dd7c495663b5e597",
+    "url": "/static/js/runtime-main.92d67ae5.js"
+  }
+]);
