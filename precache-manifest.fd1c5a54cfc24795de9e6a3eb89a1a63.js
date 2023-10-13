7cd19f6e6395ef053fe8d00ece44f1cb207f97ae
Leo J Penaloza <39505025+lpenaloza16@users.noreply.github.com>
2023-10-13 16:56:40 -0700

    core updated

--- precache-manifest.fd1c5a54cfc24795de9e6a3eb89a1a63.js
+++ precache-manifest.fd1c5a54cfc24795de9e6a3eb89a1a63.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c0cf3d0401576538d16825aac4f234b3",
+    "revision": "a2e2823398b3bdc13b0ade8a324efd38",
     "url": "/index.html"
   },
   {
-    "revision": "f6ec295d8095ff5268d6",
+    "revision": "83872981135b2f5b0e38",
     "url": "/static/css/main.4fc73c04.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.ccccdf38.chunk.js"
   },
   {
-    "revision": "f6ec295d8095ff5268d6",
-    "url": "/static/js/main.e9ace70b.chunk.js"
+    "revision": "83872981135b2f5b0e38",
+    "url": "/static/js/main.0ad85f03.chunk.js"
   },
   {
     "revision": "c365dd7c495663b5e597",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.262cf2b03e0ad3c2fadba2c271126607.js"
+  "/precache-manifest.fd1c5a54cfc24795de9e6a3eb89a1a63.js"
 );
 
 self.addEventListener('message', (event) => {
